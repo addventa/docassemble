3943616f069541a80036603551f78d2909060328
robin-lc <robin.lecorre@addventa.com>
2020-06-09 14:16:10 +0200

    travis and deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SERVER_URL=$1
+USERNAME=admin
+TARGET_DIR=/home/admin/docassemble/
+
+rsync -r --delete-after --exclude=README* --exclude=.git --exclude=scripts --quiet $TRAVIS_BUILD_DIR/ $USERNAME@$SERVER_URL:$TARGET_DIR
+ssh -t $USERNAME@$SERVER_URL \
+	'cd docassemble
+	sudo docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+	sudo docker build --tag customDocAssemble:latest .
+	sudo docker run -d -p 80:80 -p 443:443 --stop-timeout 600 customDocAssemble:latest'
