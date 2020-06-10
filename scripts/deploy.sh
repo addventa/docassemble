edda9b951bc8c40a87f4d71301b5763c4f4db6a1
Fabien <fabien.giely@addventa.com>
2020-06-10 11:14:48 +0200

    add docker stop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,8 @@ TARGET_DIR=/home/ubuntu/docassemble/
 rsync -r --delete-after --exclude=.git --exclude=scripts --quiet $TRAVIS_BUILD_DIR/ $USERNAME@$SERVER_URL:$TARGET_DIR
 ssh -t $USERNAME@$SERVER_URL \
 	'cd docassemble
+	DOCKER_STOP=$(docker ps -aq)
+	sudo docker stop ${DOCKER_STOP}
 	sudo docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
 	sudo docker build --tag docassemble_addventa:latest .
 	sudo docker run -d -p 80:80 -p 443:443 --stop-timeout 600 docassemble_addventa:latest'
