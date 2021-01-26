27cb1fc642a54cc042a4b57622fe7ecd8b63884e
Fabien <fabien.giely@addventa.com>
2021-01-26 17:13:27 +0100

    update run-run-celery.sh

--- Docker/initialize.sh
+++ Docker/initialize.sh
@@ -820,7 +820,7 @@ echo "32" >&2
 
 OTHERLOGSERVER=false
 
-if [[ $CONTAINERROLE =~ .*:(web):.* ]]; then
+if [[ $CONTAINERROLE =~ .*:(web|celery):.* ]]; then
     if [ "${LOGSERVER:-undefined}" != "undefined" ]; then
         OTHERLOGSERVER=true
     fi
@@ -860,7 +860,7 @@ fi
 
 echo "39" >&2
 
-if [[ $CONTAINERROLE =~ .*:(all):.* ]]; then
+if [[ $CONTAINERROLE =~ .*:(all|celery):.* ]]; then
     echo "checking if celery is already running..." >&2
     if su -c "source \"${DA_ACTIVATE}\" && timeout 5s celery -A docassemble.webapp.worker status" www-data 2>&1 | grep -q `hostname`; then
         echo "celery is running" >&2
@@ -874,6 +874,10 @@ else
 fi
 
 echo "40" >&2
+
+if [[ $CONTAINERROLE =~ .*:(all|celery):.* ]] && [ "$CELERYRUNNING" = false ]; then
+    supervisorctl --serverurl http://localhost:9001 start celery
+fi
 
 echo "41" >&2
 
@@ -968,6 +972,9 @@ if [ "${DAWEBSERVER:-nginx}" = "nginx" ]; then
             fi
             supervisorctl --serverurl http://localhost:9001 reread
             supervisorctl --serverurl http://localhost:9001 update
+            if [[ $CONTAINERROLE =~ .*:(all|celery):.* ]] && [ "$CELERYRUNNING" = false ]; then
+                supervisorctl --serverurl http://localhost:9001 start celery
+            fi
         fi
         echo "41.8" >&2
         if [ "${USEHTTPS:-false}" == "true" ]; then
@@ -1097,6 +1104,9 @@ if [ "${DAWEBSERVER:-nginx}" = "apache" ]; then
             fi
             supervisorctl --serverurl http://localhost:9001 reread
             supervisorctl --serverurl http://localhost:9001 update
+            if [[ $CONTAINERROLE =~ .*:(all|celery):.* ]] && [ "$CELERYRUNNING" = false ]; then
+                supervisorctl --serverurl http://localhost:9001 start celery
+            fi
         fi
 
         if [ "${BEHINDHTTPSLOADBALANCER:-false}" == "true" ]; then
