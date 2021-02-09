82e699493685e0c9f0209dba33fb34ccdf023930
Fabien <fabien.giely@addventa.com>
2021-02-09 18:15:12 +0100

    add social id in user infos

--- docassemble_base/docassemble/base/functions.py
+++ docassemble_base/docassemble/base/functions.py
@@ -683,6 +683,7 @@ def user_info():
         user.first_name = this_thread.current_info['user']['firstname']
         user.bnpp_roles = this_thread.current_info['user']['bnpp_roles']
         user.id = this_thread.current_info['user']['the_user_id']
+        user.id = this_thread.current_info['user']['social_id']
         user.last_name = this_thread.current_info['user']['lastname']
         user.email = this_thread.current_info['user']['email']
         user.country = this_thread.current_info['user']['country']
@@ -4560,7 +4561,7 @@ def set_user_info(**kwargs):
         this_thread.current_info['user']['roles'] = [y for y in kwargs['privileges']]
     if (user_id is None and email is None) or (user_id is not None and user_id == this_thread.current_info['user']['theid']) or (email is not None and email == this_thread.current_info['user']['email']):
         for key, val in kwargs.items():
-            if key in ('country', 'subdivisionfirst', 'subdivisionsecond', 'subdivisionthird', 'organization', 'timezone', 'language', 'bnpp_roles', 'id'):
+            if key in ('country', 'subdivisionfirst', 'subdivisionsecond', 'subdivisionthird', 'organization', 'timezone', 'language', 'bnpp_roles', 'id', 'social_id'):
                 this_thread.current_info['user'][key] = val
             if key == 'first_name':
                 this_thread.current_info['user']['firstname'] = val
