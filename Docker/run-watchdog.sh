fd6199ba27ecad4752042bc23723d3fa4cea4772
Fabien <fabien.giely@addventa.com>
2024-03-06 12:21:01 +0100

    populate /data/share/docassemble on startup

--- Docker/run-watchdog.sh
+++ Docker/run-watchdog.sh
@@ -3,6 +3,11 @@
 export DA_ROOT="${DA_ROOT:-/data/share/docassemble}"
 export DA_DEFAULT_LOCAL="local3.10"
 
+if [ -z "$(ls -A ${DA_ROOT})" ]; then
+  echo "copying to ${DA_ROOT}" >&2
+  cp -r /usr/share/docassemble/* ${DA_ROOT}
+fi
+
 export DA_ACTIVATE="${DA_PYTHON:-${DA_ROOT}/${DA_DEFAULT_LOCAL}}/bin/activate"
 source "${DA_ACTIVATE}"
 
